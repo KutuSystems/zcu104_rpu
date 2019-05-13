24d53bd21825d228b2eea946de3f779f1b83eb03
Greg Smart <greg.smart@kutu.com.au>
2019-05-13 22:16:40 +1000

    fixed script error

--- zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
+++ zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
@@ -3,7 +3,7 @@ createhw -name R5_0_bsp -hwspec top_ZCU104_RPU.hdf
 createapp -name R5_0_app -app {Empty Application} -proc psu_cortexr5_0 -hwproject R5_0_bsp -os standalone
 importsources -name R5_0_app -path ../../R5_0_app/src
 configbsp -bsp R5_0_app_bsp stdin psu_uart_1
-configbsp –bsp R5_0_app_bsp stdout psu_uart_1
+configbsp -bsp R5_0_app_bsp stdout psu_uart_1
 updatemss -mss R5_0_app_bsp/system.mss
 regenbsp -bsp R5_0_app_bsp
 projects -build
