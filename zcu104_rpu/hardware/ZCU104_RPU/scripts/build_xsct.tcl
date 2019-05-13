88e0fdf70b95bd7ab4f7db218dc884692298dd27
Greg Smart <greg.smart@kutu.com.au>
2019-05-13 21:32:48 +1000

    fixed rpu bsp generation

--- zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
+++ zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
@@ -2,5 +2,9 @@ setws ./
 createhw -name R5_0_bsp -hwspec top_ZCU104_RPU.hdf
 createapp -name R5_0_app -app {Empty Application} -proc psu_cortexr5_0 -hwproject R5_0_bsp -os standalone
 importsources -name R5_0_app -path ../../R5_0_app/src
+configbsp -bsp R5_0_app_bsp stdin psu_uart_1
+configbsp –bsp R5_0_app_bsp stdout psu_uart_1
+updatemss -mss R5_0_app_bsp/system.mss
+regenbsp -bsp R5_0_app_bsp
 projects -build
 exec bootgen -arch zynqmp -image ../../ZCU104_RPU/scripts/output.bif -w -o ../../../images/linux/BOOT.BIN
