f5df5bf9fbf9048b98a82c46ab7281ceb840465b
Greg Smart <greg.smart@kutu.com.au>
2019-05-13 16:21:54 +1000

    update RPU section

--- zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
+++ zcu104_rpu/hardware/ZCU104_RPU/scripts/build_xsct.tcl
@@ -3,4 +3,4 @@ createhw -name R5_0_bsp -hwspec top_ZCU104_RPU.hdf
 createapp -name R5_0_app -app {Empty Application} -proc psu_cortexr5_0 -hwproject R5_0_bsp -os standalone
 importsources -name R5_0_app -path ../../R5_0_app/src
 projects -build
-exec bootgen -arch zynqmp -image output.bif -w -o BOOT.BIN
+exec bootgen -arch zynqmp -image ../../ZCU104_RPU/scripts/output.bif -w -o ../../../images/linux/BOOT.BIN
