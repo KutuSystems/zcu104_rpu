d907f346d125f984faa52ffee8552d6ffe8e8d7b
Greg Smart <greg.smart@kutu.com.au>
2019-05-13 13:25:45 +1000

    added r5 project

---
+++ zcu104_rpu/hardware/R5_0_app/build_xsct.tcl
@@ -0,0 +1,6 @@
+setws ./
+createhw -name R5_0_bsp -hwspec top_ZCU104_RPU.hdf
+createapp -name R5_0_app -app {Empty Application} -proc psu_cortexr5_0 -hwproject R5_0_bsp -os standalone
+importsources -name R5_0_app -path ../../R5_0_app/src
+projects -build
+exec bootgen -arch zynqmp -image output.bif -w -o BOOT.BIN
